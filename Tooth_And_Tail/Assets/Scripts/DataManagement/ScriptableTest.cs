bde96a8018f25fff0339c023919c8f445b964598
BeakSeungHo <idft2008@gmail.com>
2021-12-21 10:33:27 +0900

    Reset
    Former-commit-id: 094495b55e25a85bfcbd3bce503dfb8e1927f375

--- Tooth_And_Tail/Assets/Scripts/DataManagement/ScriptableTest.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Test",menuName = "ScriptableTest", order = 1)]
-public class ScriptableTest : ScriptableObject
-{
-    public float speed;
-}
